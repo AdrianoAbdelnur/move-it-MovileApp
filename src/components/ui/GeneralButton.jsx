5d089fa62b42207df1b1cff458f083c43aa8e24e
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-06-12 15:13:32 +0930

    GeneralButton added

---
+++ src/components/ui/GeneralButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import colors from "../../styles/colors";
+
+export const GeneralButton = ({ text, onPressFunction }) => {
+  return (
+    <TouchableOpacity style={styles.GeneralButton} onPress={onPressFunction}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  GeneralButton: {
+    width: 350,
+    height: 75,
+    backgroundColor: colors.primary,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 25,
+    margin: 10,
+  },
+});
