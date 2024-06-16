38c91740b7efb736f7af83c64473ec947823208d
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-06-16 10:17:31 +0930

    TransportInfo screen added

--- src/screens/auth/register/transport/TransportInfo.jsx
+++ src/screens/auth/register/transport/TransportInfo.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   KeyboardAvoidingView,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 import globalStyles from "../../../../styles/globalStyles";
 import colors from "../../../../styles/colors";
 import { NextButton } from "../../../../components/ui/NextButton";
+import { useNavigation } from "@react-navigation/native";
+import { GeneralButton } from "../../../../components/ui/GeneralButton";
 
 export const TransportInfo = () => {
+  const navigation = useNavigation();
+  const [licensePhoto, setLicensePhoto] = useState("");
+
+  const newLicensePhoto = (newPhoto) => {
+    setLicensePhoto(newPhoto);
+  };
+
+  useEffect(() => {
+    console.log("Es este?", licensePhoto);
+  }, [licensePhoto]);
+
   return (
     <KeyboardAvoidingView style={globalStyles.KeyboardAvoidingView}>
       <View style={globalStyles.container}>
@@ -48,15 +62,19 @@ export const TransportInfo = () => {
           style={globalStyles.input}
           /* onChangeText={(value) => getInput("password", value)} */
         />
-        <TextInput
-          placeholder="confirm password"
-          keyboardType="default"
-          textContentType="password"
-          secureTextEntry={true}
-          inputMode="text"
-          style={globalStyles.input}
-          /* onChangeText={(value) => getInput("password", value)} */
+        <GeneralButton
+          text={"Photo of your license"}
+          onPressFunction={() =>
+            navigation.navigate("Camera", { newLicensePhoto })
+          }
         />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Camera", { hola: "hola" })}
+        >
+          <Text style={globalStyles.generalText}>
+            Take photo of your license
+          </Text>
+        </TouchableOpacity>
         <NextButton navigateTo={"TransportInfo"} />
       </View>
     </KeyboardAvoidingView>
