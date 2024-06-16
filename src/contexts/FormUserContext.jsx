148fb689600424a6c382f6ba519d765232d8ffc5
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-06-16 10:01:33 +0930

    FormUserContex made general

--- src/contexts/FormUserContext.jsx
+++ src/contexts/FormUserContext.jsx
@@ -5,18 +5,7 @@ export const FormUserContext = createContext();
 import React from "react";
 
 const FormUserProvider = ({ children }) => {
-  const [formData, setFormData] = useState({
-    goodsType: "",
-    dimensions: {
-      length: "",
-      width: "",
-      height: "",
-      weight: "",
-    },
-    directions: { from: "", to: "" },
-    date: "",
-    extraComents: "",
-  });
+  const [formData, setFormData] = useState({});
   return (
     <FormUserContext.Provider value={{ formData, setFormData }}>
       {children}
