1089fbc310dd2e9ca8250b3248e3a2127b05fbfe
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-08-02 14:17:14 +0930

    PostDetails modified to show new posts

--- src/screens/users/post/PostDetails.jsx
+++ src/screens/users/post/PostDetails.jsx
@@ -1,12 +1,22 @@
-import React, { useContext } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useContext, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import globalStyles from "../../../styles/globalStyles";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { useNavigation } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
+import colors from "../../../styles/colors";
 
 export const PostDetails = ({ route }) => {
   const navigation = useNavigation();
   const { state: userState } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
   const { data } = route.params;
 
@@ -27,37 +37,195 @@ export const PostDetails = ({ route }) => {
     fTime = `${hours}:${minutes.toString().padStart(2, "0")} ${ampm}`;
   };
 
+  const toggleDropdown = (currentLeg) => {
+    setIsOpen({ ...isOpen, [currentLeg]: !isOpen[currentLeg] });
+  };
+
+  const handleItemPress = (index) => {
+    setSelectedItemIndex(selectedItemIndex === index ? null : index);
+  };
+
   return (
     <View style={[globalStyles.container, { alignItems: "flex-start" }]}>
-      <Text style={globalStyles.generalInformationText}>
-        Goods type : {data?.goodsType}
-      </Text>
-      <Text style={globalStyles.generalInformationText}>Load Dimensions:</Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        height : {data?.dimensions?.height} m
-      </Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        length : {data?.dimensions?.length} m
-      </Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        width : {data?.dimensions?.width} m
-      </Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        weight : {data?.dimensions?.weight} Kg
-      </Text>
-      <Text style={globalStyles.generalInformationText}>Directions:</Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        From: {data.directions?.from?.description}
-      </Text>
-      <Text style={[globalStyles.generalInformationText, { marginLeft: 20 }]}>
-        To: {data.directions?.to?.description}
-      </Text>
-      {formatDate(data?.date)}
-      <Text style={globalStyles.generalInformationText}>date : {fDate}</Text>
-      <Text style={globalStyles.generalInformationText}>time : {fTime}</Text>
+      <Text style={globalStyles.generalText}>Title : {data?.title}</Text>
+      {formatDate(data?.date?.date)}
+      {data.date.timeDay === "specificTime" ? (
+        <Text style={globalStyles.generalText}>
+          Date: {fDate} at {fTime}
+        </Text>
+      ) : (
+        <Text style={globalStyles.generalText}>Date: {fDate}</Text>
+      )}
+      {data.date.timeDay !== "specificTime" && (
+        <Text style={globalStyles.generalText}>
+          time of day: {data.date.timeDay}
+        </Text>
+      )}
+
+      {data?.directions?.map((dir, index) => {
+        const splitedDir = dir.description.split(",");
+        const currentLeg = `leg${index + 1}`;
+        return (
+          <View key={splitedDir + index}>
+            {index == 0 ? (
+              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                From: {splitedDir[0]}
+              </Text>
+            ) : index == data?.directions.length - 1 ? (
+              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                End of the Route: {splitedDir[0]}
+              </Text>
+            ) : (
+              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                To: {splitedDir[0]}
+              </Text>
+            )}
+            {data.itemsDetails?.[currentLeg]?.description && (
+              <View style={styles.detailsContainer}>
+                <View style={styles.arrowContainer}>
+                  <View style={styles.arrowLine} />
+                  <View style={styles.arrowHeadTop} />
+                  <View style={styles.arrowHeadBottom} />
+                </View>
+                <View
+                  style={{
+                    marginLeft: 30,
+                    backgroundColor: "gray",
+                    borderRadius: 15,
+                    padding: 10,
+                    width: "80%",
+                  }}
+                >
+                  <Text style={globalStyles.generalText}>
+                    Goods Details in this leg:
+                  </Text>
+                  <Text
+                    style={[
+                      globalStyles.generalInformationText,
+                      { marginLeft: 20 },
+                    ]}
+                  >
+                    description: {data.itemsDetails?.[currentLeg]?.description}
+                  </Text>
+                  {data?.itemsDetails?.[currentLeg]?.itemsList?.length > 0 && (
+                    <View style={{ marginLeft: 20 }}>
+                      <TouchableOpacity
+                        style={styles.header}
+                        onPress={() => toggleDropdown(currentLeg)}
+                      >
+                        <Text style={globalStyles.generalInformationText}>
+                          Items List
+                        </Text>
+                        <Entypo
+                          name={
+                            isOpen[currentLeg] ? "chevron-up" : "chevron-down"
+                          }
+                          size={24}
+                          color="black"
+                        />
+                      </TouchableOpacity>
+                      {isOpen[currentLeg] && (
+                        <FlatList
+                          style={{ maxHeight: 100 }}
+                          data={data?.itemsDetails?.[currentLeg]?.itemsList}
+                          keyExtractor={(item, index) => index.toString()}
+                          renderItem={({ item, index }) => (
+                            <View>
+                              <TouchableOpacity
+                                style={styles.listItem}
+                                onPress={() => handleItemPress(index)}
+                              >
+                                <Text
+                                  style={{
+                                    color: item.details
+                                      ? colors.background
+                                      : "black",
+                                    fontSize: 13,
+                                  }}
+                                >
+                                  {item.quantity} {item.name}
+                                </Text>
+                              </TouchableOpacity>
+                              {selectedItemIndex === index && item.details && (
+                                <View style={[styles.detailsItemContainer]}>
+                                  {item?.details?.description && (
+                                    <Text
+                                      style={{
+                                        color: "blue",
+                                        flexShrink: 1,
+                                        overflow: "hidden",
+                                        textBreakStrategy: "balanced",
+                                        marginLeft: 10,
+                                      }}
+                                    >
+                                      Description: {item?.details?.description}
+                                    </Text>
+                                  )}
+                                  {item?.details?.dimensions?.length && (
+                                    <Text
+                                      style={{ color: "blue", marginLeft: 10 }}
+                                    >
+                                      length:{" "}
+                                      {item?.details?.dimensions?.length}
+                                    </Text>
+                                  )}
+                                  {item?.details?.dimensions?.width && (
+                                    <Text
+                                      style={{ color: "blue", marginLeft: 10 }}
+                                    >
+                                      width: {item?.details?.dimensions?.width}
+                                    </Text>
+                                  )}
+                                  {item?.details?.dimensions?.height && (
+                                    <Text
+                                      style={{ color: "blue", marginLeft: 10 }}
+                                    >
+                                      height:{" "}
+                                      {item?.details?.dimensions?.height}
+                                    </Text>
+                                  )}
+                                  {item?.details?.dimensions?.weight && (
+                                    <Text
+                                      style={{ color: "blue", marginLeft: 10 }}
+                                    >
+                                      weight:{" "}
+                                      {item?.details?.dimensions?.weight}
+                                    </Text>
+                                  )}
+                                </View>
+                              )}
+                            </View>
+                          )}
+                        />
+                      )}
+                    </View>
+                  )}
+                  {data.itemsDetails[currentLeg].photoItems && (
+                    <TouchableOpacity
+                      onPress={() =>
+                        navigation.navigate("Image", { currentLeg })
+                      }
+                    >
+                      <Text
+                        style={[
+                          globalStyles.generalInformationText,
+                          { marginLeft: 20, marginTop: 10, color: "blue" },
+                        ]}
+                      >
+                        View attached image
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
+              </View>
+            )}
+          </View>
+        );
+      })}
+
       {data.offerSelected && (
-        <Text style={globalStyles.generalInformationText}>
-          Transport: {data?.offerSelected?.owner?.given_name}
+        <Text style={globalStyles.generalText}>
+          Transport Selected: {data?.offerSelected?.owner?.given_name}
         </Text>
       )}
       {data.offers.find((offer) => offer?.owner?._id === userState?.user?.id)
@@ -94,3 +262,60 @@ export const PostDetails = ({ route }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    flexDirection: "row",
+  },
+  arrowContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    marginLeft: 15,
+  },
+  arrowLine: {
+    width: 2,
+    flex: 1,
+    backgroundColor: "white",
+  },
+  arrowHeadTop: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 10,
+    borderRightWidth: 10,
+    borderBottomWidth: 10,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "white",
+    position: "absolute",
+    top: 0,
+  },
+  arrowHeadBottom: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 10,
+    borderRightWidth: 10,
+    borderTopWidth: 10,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderTopColor: "white",
+    position: "absolute",
+    bottom: 0,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  listItem: {
+    /* borderBottomWidth: 1,
+    borderBottomColor: "#ddd", */
+  },
+  detailsItemContainer: {
+    width: "100%",
+    flexDirection: "column",
+  },
+});
