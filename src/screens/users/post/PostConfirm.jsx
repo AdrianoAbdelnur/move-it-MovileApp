38036d46571122acab0a187dcd2b3d60e8e04089
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-08-13 19:44:37 +0930

    ScrollView added in postConfirm

--- src/screens/users/post/PostConfirm.jsx
+++ src/screens/users/post/PostConfirm.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import {
   Alert,
-  FlatList,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -13,8 +13,11 @@ import { PostContext } from "../../../contexts/PostsContext";
 import { useNavigation } from "@react-navigation/native";
 import { FormContext } from "../../../contexts/FormContext";
 import { Entypo } from "@expo/vector-icons";
-import colors from "../../../styles/colors";
+import { EXPO_PUBLIC_GOOGLE_MAP_KEY } from "@env";
 import { GeneralButton } from "../../../components/ui/GeneralButton";
+import { ItemsListShower } from "../../../components/itemsList/ItemsListShower";
+import axios from "axios";
+import { useUpdateObj } from "../../../hooks/useUpdateObj";
 
 export const PostConfirm = () => {
   const navigation = useNavigation();
@@ -22,9 +25,20 @@ export const PostConfirm = () => {
   const { addPost, state: postsState, clearAlertMsg } = useContext(PostContext);
   const { formatDate, fDate, fTime } = useFormatDate();
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+  const [updateObj] = useUpdateObj(setFormData);
 
   useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  useEffect(() => {
+    for (let i = 0; i < formData?.directions?.length - 1; i++) {
+      getRouteInfo(
+        formData?.directions[i].place_id,
+        formData?.directions[i + 1].place_id,
+        (currentLeg = `leg${i + 1}`)
+      );
+    }
     if (formData.date.date) {
       formatDate(formData.date.date);
     }
@@ -61,180 +75,136 @@ export const PostConfirm = () => {
     setIsOpen({ ...isOpen, [currentLeg]: !isOpen[currentLeg] });
   };
 
-  const handleItemPress = (index) => {
-    setSelectedItemIndex(selectedItemIndex === index ? null : index);
+  const getRouteInfo = async (origin, destination, currentLeg) => {
+    console.log(origin, destination, currentLeg);
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=place_id:${origin}&destination=place_id:${destination}&key=${EXPO_PUBLIC_GOOGLE_MAP_KEY}`
+      );
+      const route = response.data.routes[0];
+      const distance = route.legs[0].distance.text;
+      const duration = route.legs[0].duration.text;
+      updateObj(`itemsDetails.${currentLeg}.duration`, duration);
+      updateObj(`itemsDetails.${currentLeg}.distance`, distance);
+    } catch (error) {
+      console.log("Error fetching route details:", error);
+    }
   };
 
   return (
     <View style={[globalStyles.container, { alignItems: "flex-start" }]}>
-      <Text style={globalStyles.generalText}>Title : {formData?.title}</Text>
-      <Text style={globalStyles.generalText}>date : {fDate}</Text>
-      <Text style={globalStyles.generalText}>time : {fTime}</Text>
-      {formData?.directions?.map((dir, index) => {
-        const splitedDir = dir.description.split(",");
-        const currentLeg = `leg${index + 1}`;
-        return (
-          <View key={splitedDir + index}>
-            {index == 0 ? (
-              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
-                From: {splitedDir[0]}
-              </Text>
-            ) : index == formData?.directions.length - 1 ? (
-              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
-                End of the Route: {splitedDir[0]}
-              </Text>
-            ) : (
-              <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
-                To: {splitedDir[0]}
-              </Text>
-            )}
-            {formData.itemsDetails?.[currentLeg]?.description && (
-              <View style={styles.detailsContainer}>
-                <View style={styles.arrowContainer}>
-                  <View style={styles.arrowLine} />
-                  <View style={styles.arrowHeadTop} />
-                  <View style={styles.arrowHeadBottom} />
-                </View>
-                <View
-                  style={{
-                    marginLeft: 30,
-                    backgroundColor: "gray",
-                    borderRadius: 15,
-                    padding: 10,
-                    width: "80%",
-                  }}
-                >
-                  <Text style={globalStyles.generalText}>
-                    Goods Details in this leg:
-                  </Text>
-                  <Text
-                    style={[
-                      globalStyles.generalInformationText,
-                      { marginLeft: 20 },
-                    ]}
+      <ScrollView style={{ width: "100%" }}>
+        <Text style={globalStyles.generalText}>Title : {formData?.title}</Text>
+        <Text style={globalStyles.generalText}>date : {fDate}</Text>
+        <Text style={globalStyles.generalText}>time : {fTime}</Text>
+        {formData?.directions?.map((dir, index) => {
+          const splitedDir = dir.description.split(",");
+          const currentLeg = `leg${index + 1}`;
+          return (
+            <View key={splitedDir + index}>
+              {index == 0 ? (
+                <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                  From: {splitedDir[0]}
+                </Text>
+              ) : index == formData?.directions.length - 1 ? (
+                <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                  End of the Route: {splitedDir[0]}
+                </Text>
+              ) : (
+                <Text style={[globalStyles.generalText, { marginBottom: 0 }]}>
+                  To: {splitedDir[0]}
+                </Text>
+              )}
+              {formData.itemsDetails?.[currentLeg]?.description && (
+                <View style={styles.detailsContainer}>
+                  <View style={styles.arrowContainer}>
+                    <View style={styles.arrowLine} />
+                    <View style={styles.arrowHeadTop} />
+                    <View style={styles.arrowHeadBottom} />
+                  </View>
+                  <View
+                    style={{
+                      marginLeft: 30,
+                      backgroundColor: "gray",
+                      borderRadius: 15,
+                      padding: 10,
+                      width: "80%",
+                    }}
                   >
-                    description:{" "}
-                    {formData.itemsDetails?.[currentLeg]?.description}
-                  </Text>
-                  {formData?.itemsDetails?.[currentLeg]?.itemsList?.length >
-                    0 && (
-                    <View style={{ marginLeft: 20 }}>
-                      <TouchableOpacity
-                        style={styles.header}
-                        onPress={() => toggleDropdown(currentLeg)}
-                      >
-                        <Text style={globalStyles.generalInformationText}>
-                          Items List
-                        </Text>
-                        <Entypo
-                          name={
-                            isOpen[currentLeg] ? "chevron-up" : "chevron-down"
-                          }
-                          size={24}
-                          color="black"
-                        />
-                      </TouchableOpacity>
-                      {isOpen[currentLeg] && (
-                        <FlatList
-                          style={{ maxHeight: 100 }}
-                          data={formData?.itemsDetails?.[currentLeg]?.itemsList}
-                          keyExtractor={(item, index) => index.toString()}
-                          renderItem={({ item, index }) => (
-                            <View>
-                              <TouchableOpacity
-                                style={styles.listItem}
-                                onPress={() => handleItemPress(index)}
-                              >
-                                <Text
-                                  style={{
-                                    color: item.details
-                                      ? colors.background
-                                      : "black",
-                                    fontSize: 13,
-                                  }}
-                                >
-                                  {item.quantity} {item.name}
-                                </Text>
-                              </TouchableOpacity>
-                              {selectedItemIndex === index && item.details && (
-                                <View style={[styles.detailsItemContainer]}>
-                                  {item?.details?.description && (
-                                    <Text
-                                      style={{
-                                        color: "blue",
-                                        flexShrink: 1,
-                                        overflow: "hidden",
-                                        textBreakStrategy: "balanced",
-                                        marginLeft: 10,
-                                      }}
-                                    >
-                                      Description: {item?.details?.description}
-                                    </Text>
-                                  )}
-                                  {item?.details?.dimensions?.length && (
-                                    <Text
-                                      style={{ color: "blue", marginLeft: 10 }}
-                                    >
-                                      length:{" "}
-                                      {item?.details?.dimensions?.length}
-                                    </Text>
-                                  )}
-                                  {item?.details?.dimensions?.width && (
-                                    <Text
-                                      style={{ color: "blue", marginLeft: 10 }}
-                                    >
-                                      width: {item?.details?.dimensions?.width}
-                                    </Text>
-                                  )}
-                                  {item?.details?.dimensions?.height && (
-                                    <Text
-                                      style={{ color: "blue", marginLeft: 10 }}
-                                    >
-                                      height:{" "}
-                                      {item?.details?.dimensions?.height}
-                                    </Text>
-                                  )}
-                                  {item?.details?.dimensions?.weight && (
-                                    <Text
-                                      style={{ color: "blue", marginLeft: 10 }}
-                                    >
-                                      weight:{" "}
-                                      {item?.details?.dimensions?.weight}
-                                    </Text>
-                                  )}
-                                </View>
-                              )}
-                            </View>
-                          )}
-                        />
-                      )}
-                    </View>
-                  )}
-                  {formData.itemsDetails[currentLeg].photoItems && (
-                    <TouchableOpacity
-                      onPress={() =>
-                        navigation.navigate("Image", {
-                          image: formData.itemsDetails[currentLeg].photoItems,
-                        })
-                      }
+                    <Text style={globalStyles.generalText}>
+                      Goods Details in this leg:
+                    </Text>
+                    <Text
+                      style={[
+                        globalStyles.generalInformationText,
+                        { marginLeft: 20 },
+                      ]}
                     >
-                      <Text
-                        style={[
-                          globalStyles.generalInformationText,
-                          { marginLeft: 20, marginTop: 10, color: "blue" },
-                        ]}
+                      description:{" "}
+                      {formData.itemsDetails?.[currentLeg]?.description}
+                    </Text>
+                    {formData?.itemsDetails?.[currentLeg]?.itemsList?.length >
+                      0 && (
+                      <View style={{ marginLeft: 20 }}>
+                        <TouchableOpacity
+                          style={styles.header}
+                          onPress={() => toggleDropdown(currentLeg)}
+                        >
+                          <Text style={globalStyles.generalInformationText}>
+                            Items List
+                          </Text>
+                          <Entypo
+                            name={
+                              isOpen[currentLeg] ? "chevron-up" : "chevron-down"
+                            }
+                            size={24}
+                            color="black"
+                          />
+                        </TouchableOpacity>
+                        {isOpen[currentLeg] &&
+                          formData?.itemsDetails?.[currentLeg]?.itemsList.map(
+                            (item, index) => (
+                              <ItemsListShower
+                                key={index}
+                                item={item}
+                                index={index}
+                              />
+                            )
+                          )}
+                      </View>
+                    )}
+                    {formData.itemsDetails[currentLeg].photoItems && (
+                      <TouchableOpacity
+                        onPress={() =>
+                          navigation.navigate("Image", {
+                            image: formData.itemsDetails[currentLeg].photoItems,
+                          })
+                        }
                       >
-                        View attached image
-                      </Text>
-                    </TouchableOpacity>
-                  )}
+                        <Text
+                          style={[
+                            globalStyles.generalInformationText,
+                            { marginLeft: 20, marginTop: 10, color: "blue" },
+                          ]}
+                        >
+                          View attached image
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    <Text>
+                      distance: {formData.itemsDetails[currentLeg].distance}
+                    </Text>
+                    <Text>
+                      duration: {formData.itemsDetails[currentLeg].duration}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            )}
-          </View>
-        );
-      })}
-      <GeneralButton text="Confirm" onPressFunction={onSubmit} />
+              )}
+            </View>
+          );
+        })}
+        <GeneralButton text="Confirm" onPressFunction={onSubmit} />
+      </ScrollView>
     </View>
   );
 };
